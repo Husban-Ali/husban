3bba479386c2ef48ee95670fae5f32a0eebfb72f
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-12 11:02:47 +0500

    hero-section-completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,30 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { Download,Send } from "lucide-react"
-import {RiBriefcase4Fill,RiTeamFill,RiTodoFill,RiArrowDownSLine} from 'react-icons/ri'
+import { Download, Send } from "lucide-react"
+import { RiBriefcase4Fill, RiTeamFill, RiTodoFill, RiArrowDownSLine } from 'react-icons/ri'
 
 import { DevImage } from "./DevImage"
+import { Badge } from "./Badge"
+import { Social } from "./Social"
+
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="py-12 xl:py-26 h-[84vh] xl:pt-28  w-full bg-white dark:bg-[#0b0908] bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Full Stack Developer</div>
+            <h1 className="h1">Hello,My Name is Hamiz Muzaffer</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores delectus nostrum facere, temporibus accusamus ea, vitae odit ipsa rerum, consequuntur architecto eveniet maiores fuga molestias aspernatur? Ratione recusandae sint eius.</p>
+            <div>buttons</div>
+          </div>
+          <div className="hidden xl-flex relative">image</div>
+        </div>
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary cursor-pointer" />
+        </div>
+      </div>
+
+    </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { Urbanist } = require('next/font/google');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -71,6 +73,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+
+      backgroundImage : {
+        hero : 'url(/hero/hero-bg.png)'
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
