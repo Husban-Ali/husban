bb8ba58d58fa53891a7051db8bdf314ff1b858f8
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-13 21:38:39 +0500

    vercel-error-fixed

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,30 +1,32 @@
-import { Sheet,SheetContent,SheetTrigger,SheetClose } from './ui/sheet'
+import { Sheet, SheetContent, SheetTrigger, SheetClose,SheetTitle } from './ui/sheet'
 import { AlignJustify } from 'lucide-react'
 import { Nav } from './Nav'
 import { Logo } from './Logo'
 import { Social } from './Social'
 import { useState } from 'react'
+
 export const MobileNav = () => {
   const [open, setOpen] = useState(false)
 
   const handleClose = () => {
     setOpen(false)
   }
+
   return (
     <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild >
+      <SheetTrigger asChild>
         <AlignJustify className='cursor-pointer' />
       </SheetTrigger>
       <SheetContent>
+        <SheetTitle className="sr-only">Mobile Navigation</SheetTitle>
         <div className='flex flex-col items-center justify-between h-full py-8'>
           <div className='flex flex-col items-center gap-y-12'>
-          <SheetClose asChild>
+            <SheetClose asChild>
               <div onClick={handleClose}>
                 <Logo />
               </div>
             </SheetClose>
-            <Nav  containerStyles="flex flex-col items-center pt-4 gap-y-6 font-light" linkStyles="text-2xl" onNavClick={handleClose}/>
-          
+            <Nav containerStyles="flex flex-col items-center pt-4 gap-y-6 font-light" linkStyles="text-2xl" onNavClick={handleClose}/>
           </div>
           <Social containerStyles="flex gap-x-4" iconsStyles="text-2xl"/>
         </div>
